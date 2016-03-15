2408aed06321a67f73726726ab5ece5224f1ac3e
bmeduho <bmeduho@users.noreply.github.com>
2016-03-15 14:20:54 -0400

    Update Type.js

--- Code/Type.js
+++ Code/Type.js
@@ -1,1 +1,34 @@
-
+var Types = {
+	/*
+	Name: {
+		Strong: [],
+		Weak: [],
+		Immune: []
+	},
+	*/
+	Normal: {
+		Strong: [false],
+		Weak: ["Rock","Steel"],
+		Immune: ["Ghost"]
+	},
+	Fire: {
+		Strong: ["Grass","Ice","Bug","Steel"],
+		Weak: ["Fire","Water","Rock","Dragon"],
+		Immune: [false]
+	},
+	Water: {
+		Strong: ["Fire","Ground","Rock"],
+		Weak: ["Water","Grass","Dragon"],
+		Immune: [false]
+	},
+	Electric: {
+		Strong: ["Water","Flying"],
+		Weak: ["Electric","Grass","Dragon"],
+		Immune: ["Ground"]
+	},
+	Grass: {
+		Strong: ["Water","Ground","Rock"],
+		Weak: ["Fire","Grass","Poison","Fire","Bug","Dragon","Steel"],
+		Immune: [false]
+	},
+}

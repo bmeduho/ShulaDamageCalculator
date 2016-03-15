d339f9e0c36edd03b7249d410ecea0b18e2247b0
bmeduho <bmeduho@users.noreply.github.com>
2016-03-15 13:51:11 -0400

    Update Calculation.js

--- Code/Calculation.js
+++ Code/Calculation.js
@@ -1,1 +1,22 @@
+function HPStat (Level, Pokemon) {
+	
+}
+function MainStats (Level, Pokemon, Stat) {
+	
+}
+function Damage (Level, Pokemon, Move) {
+	var AtkStat = 
+	var stepOne = ((2 * Level / 5) + 2);
+	var stepTwo = 
+}
 
+
+
+Damage
+	((((2 * Level / 5 + 2) * AttackStat * AttackPower / DefenseStat) / 50) + 2) * STAB * Weakness/Resistance * RandomNumber / 100
+
+Hit Points:
+        ( (IV + 2 * BaseStat + (EV/4) ) * Level/100 ) + 10 + Level
+
+Attack, Defense, Speed, Sp. Attack, Sp. Defense:
+        (((IV + 2 * BaseStat + (EV/4) ) * Level/100 ) + 5)
